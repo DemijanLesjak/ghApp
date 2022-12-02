f3a8c7ddad4008f39c9a73aff5fbcb9511a96cdc
DemijanLesjak <demi.lesjak@gmail.com>
2022-12-02 13:28:01 +0100

    Prepare main input structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CurrencyDropdownComponent } from './components/currency-dropdown/currency-dropdown.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyDropdownComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
